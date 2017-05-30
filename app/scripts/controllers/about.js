805ee577bd4e60c0466cb7a4cbc8959c57cccdb2
Santiago Garces Gonzalez <sgarcesg1@gmail.com>
2017-05-29 21:41:48 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name olimpoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the olimpoApp
+ */
+angular.module('olimpoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name olimpoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the olimpoApp
+ */
+angular.module('olimpoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
